647f5fc87eec8a6739851abbbe8194eed3cada09
ajay datla <ajaydatlalbn06@gmail.com>
2018-07-28 11:39:27 +0530

    project started

---
+++ audits/src/main/java/com/ca/audits/AuditsApplication.java
@@ -0,0 +1,12 @@
+package com.ca.audits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditsApplication.class, args);
+	}
+}
