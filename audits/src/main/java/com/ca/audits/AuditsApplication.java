2e0322126b084cbc26556dfe61800542ab3d1b8c
ajay datla <ajaydatlalbn06@gmail.com>
2018-07-28 11:51:57 +0530

    SpringBootServletInitializer

--- audits/src/main/java/com/ca/audits/AuditsApplication.java
+++ audits/src/main/java/com/ca/audits/AuditsApplication.java
@@ -2,9 +2,10 @@ package com.ca.audits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AuditsApplication {
+public class AuditsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuditsApplication.class, args);
