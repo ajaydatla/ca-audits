c618462f800e5fc85cf105948af14fca23a56b3e
ajaydatla <ajaydatlalbn06@gmail.com>
2018-11-04 16:11:10 +0530

    side bar finished almost

--- audits/src/main/resources/static/customJS/userhome.js
+++ audits/src/main/resources/static/customJS/userhome.js
@@ -1,14 +1,23 @@
-$(function () {
-    $(".sidebar-list").mouseenter(function () {
-        var eid = $(this).attr('id');
-        console.log("called fddun d ", eid);
-        var addele = "<ul class='nav nav-pills nav-stacked sidebar-dropdown'>"+
-        "<li><a href='#'>hello</a></li><li><a href='#'>world</a></li></ul>";
-        $("#inner-" + eid).append(addele);
-        $("#inner-" + eid).show();
-    });
-    $(".sidebar-list").mouseleave(function () {
-        var eid = $(this).attr('id');
-        $("#inner-" + eid).empty();
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    
+    .when("/clientcontacts", {
+        templateUrl : "clientcontacts"
     });
 });
+// $(function () {
+//     $(".sidebar-list").mouseenter(function () {
+//         var eid = $(this).attr('id');
+//         console.log("called fddun d ", eid);
+//         var addele = "<ul class='nav nav-pills nav-stacked sidebar-dropdown'>"+
+//         "<li><a href='#'>hello</a></li><li><a href='#'>world</a></li></ul>";
+//         $("#inner-" + eid).append(addele);
+//         $("#inner-" + eid).show();
+//     });
+//     $(".sidebar-list").mouseleave(function () {
+//         var eid = $(this).attr('id');
+//         $("#inner-" + eid).empty();
+//     });
+// });
+
